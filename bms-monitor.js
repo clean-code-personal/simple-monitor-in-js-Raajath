4211c621954085f5c9bc68339331c4f1011b290e
Raajath <rajathn02@gmail.com>
2024-02-13 13:50:14 +0530

    optimization done Created seperate function for redundant code

--- bms-monitor.js
+++ bms-monitor.js
@@ -5,16 +5,21 @@ function checkError(inputParameter,limits){
     return inputParameter<limits.minimum || inputParameter>limits.maximum;
 }
 
+function getLog(booleanValue,inputParameterType)
+{
+    return booleanValue ? `${inputParameterType} is out of range!`:'';
+}
+
 function batteryIsOk(temperature, soc, charge_rate,limits) {
 
   
-   const temperatureError=checkError(temperature,limits.temperature);
-   const socError=checkError(soc,limits.soc);
-   const chargeRateError=checkError(charge_rate,limits.chargeRate);
+    const temperatureError=checkError(temperature,limits.temperature);
+    const socError=checkError(soc,limits.soc);
+    const chargeRateError=checkError(charge_rate,limits.chargeRate);
 
-    const temperatureErrorLog=temperatureError?'Temperature is out of range!':'';
-    const socErrorLog=socError?'State of Charge is out of range!':'';
-    const chargeRateErrorLog=chargeRateError ?'Charge rate is out of range!':'';
+    const temperatureErrorLog=getLog(temperatureError,"Temperature");
+    const socErrorLog=getLog(socError,"State of Battery")
+    const chargeRateErrorLog=getLog(chargeRateError,"Charge Rate");
 
     //used filter to remove empty logs
     const errorLogs=[temperatureErrorLog,socErrorLog,chargeRateErrorLog].filter(
@@ -30,4 +35,4 @@ function batteryIsOk(temperature, soc, charge_rate,limits) {
 
 
 
-console.log(batteryIsOk(40,66,0.6,limits))
+console.log(batteryIsOk(40,76,0.7,limits))
