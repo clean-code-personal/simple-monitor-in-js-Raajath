badd90aa9e38e72a17a12c76233751e9bc8a3a33
Raajath <rajathn02@gmail.com>
2024-02-13 13:27:22 +0530

    tried to reduce cyclometric complexity

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,33 @@
 const {expect} = require('chai');
+const {limits}=require('./inputParameterLimits');
 
-function batteryIsOk(temperature, soc, charge_rate) {
-    if (temperature < 0 || temperature > 45) {
-        console.log('Temperature is out of range!');
-        return false;
-    } else if (soc < 20 || soc > 80) {
-        console.log('State of Charge is out of range!')
-        return false;
-    } else if (charge_rate > 0.8) {
-        console.log('Charge rate is out of range!');
-        return false;
-    }
-    return true;
+function checkError(inputParameter,limits){
+    return inputParameter<limits.minimum || inputParameter>limits.maximum;
 }
 
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(50, 85, 0)).to.be.false;
+function batteryIsOk(temperature, soc, charge_rate,limits) {
+
+  
+   const temperatureError=checkError(temperature,limits.temperature);
+   const socError=checkError(soc,limits.soc);
+   const chargeRateError=checkError(charge_rate,limits.chargeRate);
+
+    const temperatureErrorLog=temperatureError?'Temperature is out of range!':'';
+    const socErrorLog=socError?'State of Charge is out of range!':'';
+    const chargeRateErrorLog=chargeRateError ?'Charge rate is out of range!':'';
+
+    //used filter to remove empty logs
+    const errorLogs=[temperatureErrorLog,socErrorLog,chargeRateErrorLog].filter(
+        (log)=>{
+            return log!='';
+        }
+
+    );
+  
+    return errorLogs.length==0?'Battery is good':errorLogs.join('\n');
+
+}
+
+
+
+console.log(batteryIsOk(40,66,0.6,limits))
---
+++ inputParameterLimits.js
@@ -0,0 +1,19 @@
+
+const limits={
+ 
+    temperature:{
+        minimum:0,
+        maximum:45
+    },
+    soc:{
+        minimum:20,
+        maximum:80
+    },
+    chargeRate:{
+        minimum:0.0,
+        maximum:.8
+    }
+    
+};
+
+module.exports={limits};
