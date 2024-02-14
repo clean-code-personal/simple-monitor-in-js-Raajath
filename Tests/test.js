adec1afeb20b7f8f233c6776406fe74c838085f8
Raajath <rajathn02@gmail.com>
2024-02-14 22:26:37 +0530

    Tried adding Warning messages

--- Tests/test.js
+++ Tests/test.js
@@ -1,93 +1,103 @@
 const { batteryStatus } = require("../bms-monitor");
-const {limits}=require('../inputParameterLimits');
-const {outPutLogs}=require('../outputLogs');
+const {rangeLog}=require('../rangeMessages');
 const assert =require('assert');
 const {describe,it}=require('mocha');
 
-const testBatteryCondition=batteryStatus(limits,outPutLogs);
+const testBatteryCondition=batteryStatus(rangeLog);
+const testCases={
+test1:{
+    temperature:40,
+    soc:70,
+    chargeRate:0.7,
+    expectedMessage:"Battery is good"
+},
+test2:{
+    temperature:50,
+    soc:70,
+    chargeRate:0.7,
+    expectedMessage:"Temperature is high"
+},
+test3:{
+    temperature:40,
+    soc:90,
+    chargeRate:0.7,
+    expectedMessage:"State of Battery is high"
+},
+test4:{
+    temperature:40,
+    soc:70,
+    chargeRate:0.9,
+    expectedMessage:"Charge Rate is high"
+},
+test5:{
+    temperature:50,
+    soc:90,
+    chargeRate:0.7,
+    expectedMessage:"Temperature is high\nState of Battery is high"
+},
+test6:{
+    temperature:50,
+    soc:10,
+    chargeRate:0.7,
+    expectedMessage:"Temperature is high\nState of Battery is low"
+},
 
+test7:{
+    temperature:-1,
+    soc:10,
+    chargeRate:0.01,
+    expectedMessage:"Temperature is low\nState of Battery is low\nCharge Rate is low"
+},
+test8:{
+    temperature:50,
+    soc:95,
+    chargeRate:0.9,
+    expectedMessage:"Temperature is high\nState of Battery is high\nCharge Rate is high"
+},
+
+test9:{
+    temperature:2,
+    soc:22,
+    chargeRate:0.07,
+    expectedMessage:"Temperature Low-Warning\nState of Battery Low-Warning\nCharge Rate Low-Warning"
+     
+},
+test10:{
+    temperature:44,
+    soc:78,
+    chargeRate:0.78,
+    expectedMessage:"Temperature High-Warning\nState of Battery High-Warning\nCharge Rate High-Warning"
+     
+},
+
+test11:{
+    temperature:200,
+    soc:110,
+    chargeRate:1.1,
+    expectedMessage:"Out of Bound\nOut of Bound\nOut of Bound"
+     
+},
+test12:{
+    temperature:-200,
+    soc:-1,
+    chargeRate:-1,
+    expectedMessage:"Out of Bound\nOut of Bound\nOut of Bound"
+     
+},
+
+
+}
 
 describe('Battery conditions all tests',function(){
 
-
-describe('Battery Condition Good Test', function() {
-    it('Battery is good Input conditions ', function() {
-      assert.strictEqual(testBatteryCondition.batteryIsOk(40, 70, 0.7), "Battery is good");
-    });
-  });
-
-
-  describe('When one of battery parameter is out of range',function(){
- 
-    it('Teperature is high in this test case',function(){
-    assert.strictEqual(testBatteryCondition.batteryIsOk(50,70,0.8),"Temperature is high");
-    });
-
-    it('Temperature is low in this test case',function(){
-    assert.strictEqual(testBatteryCondition.batteryIsOk(-1,70,0.8),"Temperature is low");
-    });
-
-    it('State of Battery is high in this test case',function(){
-        assert.strictEqual(testBatteryCondition.batteryIsOk(40,90,0.8),"State of Battery is high");
-    });
-
-    it('State of Battery is low in this test case"',function(){
-        assert.strictEqual(testBatteryCondition.batteryIsOk(40,10,0.8),"State of Battery is low");
-    });
-
-    it('Charge Rate is high in this test case',function(){
-        assert.strictEqual(testBatteryCondition.batteryIsOk(40,70,0.9),"Charge Rate is high");
-    });
-
-
-  });
-
-
-  describe('When multiple parameter is out of range',function(){
-
-    it('Temperature and Charge rate high here',function(){
-
-        assert.strictEqual(testBatteryCondition.batteryIsOk(50,70,0.9),"Temperature is high\nCharge Rate is high");
-    });
-
-    it('Temperature and state of charge is  high here',function(){
-
-        assert.strictEqual(testBatteryCondition.batteryIsOk(50,90,0.7),"Temperature is high\nState of Battery is high");
-    });
-
-    
-    it('Temperature and state of charge is  low here',function(){
-
-        assert.strictEqual(testBatteryCondition.batteryIsOk(-1,10,0.7),"Temperature is low\nState of Battery is low");
-    });
-
-    it('Temperature is high and state of charge is  low here',function(){
-
-        assert.strictEqual(testBatteryCondition.batteryIsOk(50,10,0.7),"Temperature is high\nState of Battery is low");
-    });
-
-    it('Temperature is low and state of charge is  high here',function(){
-
-        assert.strictEqual(testBatteryCondition.batteryIsOk(-1,92,0.7),"Temperature is low\nState of Battery is high");
-    });
-
-    it('State of battery is high and Charge rate is high',function(){
-   assert.strictEqual(testBatteryCondition.batteryIsOk(40,90,0.9),"State of Battery is high\nCharge Rate is high");
-    });
-
-    it('All parameters high',function(){
-        assert.strictEqual(testBatteryCondition.batteryIsOk(50,90,0.9),"Temperature is high\nState of Battery is high\nCharge Rate is high");
-    });
-
-    it('All parameters low',function(){
-        assert.strictEqual(testBatteryCondition.batteryIsOk(-1,10,-1),"Temperature is low\nState of Battery is low\nCharge Rate is low");
-    });
-
-
-  });
-
-
-
+ it("Tests all cases",function(){
+     
+   for(testNumber in testCases)
+   {
+   const test=testCases[testNumber];
+   assert.strictEqual(testBatteryCondition.batteryIsOk(test.temperature, test.soc, test.chargeRate), test.expectedMessage);
+   }
+});
 
 });
 
